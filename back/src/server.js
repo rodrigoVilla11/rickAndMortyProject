4e266b0a86c3df801076886c687c14a12753bd36
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-04-06 12:09:57 -0300

    Listo Parte 1 de HW sequelize

--- back/src/server.js
+++ back/src/server.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
-const express = require('express');
-const router = require('./routes');
-const morgan = require('morgan')
-const cors = require("cors")
+const express = require("express");
+const router = require("./routes");
+const morgan = require("morgan");
+const cors = require("cors");
+const { conn } = require("./DB_connection");
 
 const server = express();
-server.use(express.json())
-server.use(morgan('dev'));
-server.use(cors())
+server.use(express.json());
+server.use(morgan("dev"));
+server.use(cors());
 
-server.use('/rickandmorty', router)
+server.use("/rickandmorty", router);
 
 const PORT = process.env.PORT || 3001;
 //res.setHeader('Access-Control-Allow-Origin', '*')
 
-server.listen(PORT, ()=>{
-  console.log(`listening on port ${PORT}`);
-})
+server.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+	conn.sync();
+});
