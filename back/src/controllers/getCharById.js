fbb897ebeab846df04bab60cb4c6cbd5636bcb1c
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-11-17 23:52:24 -0300

    utilizando api publica

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -1,12 +1,11 @@
 const axios = require("axios");
 //const {KEY, URL} = process.env
-const URL = "http://be-a-rym.up.railway.app/api";
-const KEY = "86334f583361.82f001e1c09131539161";
+const URL = "https://rickandmortyapi.com/api";
 
 const getCharById = async (req, res) => {
 	try {
 		const { id } = req.params;
-		const response = await axios.get(`${URL}/character/${id}?key=${KEY}`);
+		const response = await axios.get(`${URL}/character/${id}`);
 		const { id: charId, name, species, image, gender } = response.data;
 		res.status(200).json({ id, name, species, image, gender });
 	} catch (error) {
