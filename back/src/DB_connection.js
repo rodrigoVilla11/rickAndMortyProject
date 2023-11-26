904b731026dcc428eea90a913afaf515564646a6
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-11-26 18:35:40 -0300

    db_connection

--- back/src/DB_connection.js
+++ back/src/DB_connection.js
@@ -6,7 +6,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 // URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
 const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/verceldb`,
 	{ logging: false, native: false }
 );
 
